50d11052c02e1f50aeaa24e75706d9df3bf09e12
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-21 15:12:42 -0400

    Add JUnit5; Parameterized existing test cases

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -8,11 +10,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AppTest {
-    @Test
-    public void test() {
-        String inputDate = "2018-11-30";
-        String expectedOutputDate = "November 30 2018";
-
+    @DisplayName("utcDateToCustomFormat")
+    @ParameterizedTest(name = "given UTC date: {0}, it returns custom formatted date {1}")
+    @CsvSource({
+            "2018-11-30, November 30 2018",
+            "1969-01-01, January 01 1969"
+    })
+    public void test(String inputDate, String expectedOutputDate) {
         String inputFormat = "yyyy-MM-dd";
         String outputFormat = "MMMM dd yyyy";
 
